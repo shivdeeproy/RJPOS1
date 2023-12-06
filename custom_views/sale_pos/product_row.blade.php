6562def091389baa3ce7cc281b5567e4f6815771
shivdeeproy <shivdeep.roy@gmail.com>
2023-12-06 12:18:46 +0530

    check

--- custom_views/sale_pos/product_row.blade.php
+++ custom_views/sale_pos/product_row.blade.php
@@ -333,7 +333,6 @@
 				$pos_unit_price = $so_line->unit_price_before_discount;
 			}
 		@endphp
-
 		<td class="@if(!auth()->user()->can('edit_product_price_from_sale_screen')) hide @endif">
 			<input type="text" name="products[{{$row_count}}][unit_price]" class="form-control pos_unit_price input_number mousetrap" value="{{@num_format($pos_unit_price)}}" @if(!empty($pos_settings['enable_msp'])) data-rule-min-value="{{$pos_unit_price}}" data-msg-min-value="{{__('lang_v1.minimum_selling_price_error_msg', ['price' => @num_format($pos_unit_price)])}}" @endif> 
 
@@ -378,12 +377,6 @@
 			</td>
 		@endif
 	@endif
-	<td class="{{$hide_tax}}">
-		<input type="text" name="products[{{$row_count}}][mrp_inc_tax]" class="form-control mrp_inc_tax input_number" value="{{@num_format($product->mrp_inc_tax)}}" @if(!$edit_price) readonly @endif >
-	</td>
-	<td class="{{$hide_tax}}">
-		<input type="text" name="products[{{$row_count}}][discount]" class="form-control discount input_number" value="{{@num_format($product->discount)}}" @if(!$edit_price) readonly @endif >
-	</td>
 	<td class="{{$hide_tax}}">
 		<input type="text" name="products[{{$row_count}}][unit_price_inc_tax]" class="form-control pos_unit_price_inc_tax input_number" value="{{@num_format($unit_price_inc_tax)}}" @if(!$edit_price) readonly @endif @if(!empty($pos_settings['enable_msp'])) data-rule-min-value="{{$unit_price_inc_tax}}" data-msg-min-value="{{__('lang_v1.minimum_selling_price_error_msg', ['price' => @num_format($unit_price_inc_tax)])}}" @endif>
 	</td>
